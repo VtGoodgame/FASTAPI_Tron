e22384567ad475794d378515718a9f85160fa716
inessa <devisheva.ina@gmail.com>
2025-07-22 15:40:29 +0400

    first commit

---
+++ src/consts.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+#DB
+BACKEND_URL = 'localhost:8000'
+DB_HOST=os.getenv('DB_HOST')
+DB_PORT=os.getenv('DB_PORT')
+DB_USER=os.getenv('DB_USER')
+DB_PASSWORD=os.getenv('DB_PASSWORD')
+DB_SCHEMA = os.getenv('DB_SCHEMA')
+DB_NAME=os.getenv('DB_NAME')
+
+COOKIE_NAME = "access_token"
+
+REDIS_HOST = os.getenv('REDIS_HOST')
+REDIS_PORT = os.getenv('REDIS_PORT')
+REDIS_PASSWORD = os.getenv('REDIS_PASSWORD')
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = "HS256"
